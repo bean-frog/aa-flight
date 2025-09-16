4e7695df026b47e049c6862def2559b067df8b2a
beanfrog <gkieran32855@gmail.com>
2025-09-16 10:05:29 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # aa-flight
 query flight information from aboard an american airlines flight
+--
+I was bored on an American Airlines flight and decided to poke around with the network devtools on aainflight.com. </br>
+Turns out, the endpoint they use to show you flight progress has a lot more data that they dont show you. </br>
+I dont include every data point in this script but its very easy to add more if you want. </br>
+The endpoint is only accessible while connected to the aainflight.com wifi network, so you have to be on the plane to use the script. </br>
+
+## To use
+- make sure you have nodejs and the dependencies installed before you board, unless you're fine with paying $20 for internet access.
+- to install dependencies use `npm install`
+- `node fetchinfo.js` will fetch the data once and exit
+- `node fetchinfo-continuous.js` will clear the screen and fetch every few seconds.
---
+++ fetchinfo-continuous.js
@@ -0,0 +1,81 @@
+// American Airlines flight data
+// Fetches data of current flight (continuous version).
+// Must be connected to aainflight wifi
+
+import fetch from 'node-fetch';
+
+// ANSI color codes
+const RED = '\x1b[31m'
+const GREEN = '\x1b[32m';
+const WHITE = '\x1b[37m';
+const BOLD = '\x1b[1m';
+const RESET = '\x1b[0m';
+
+// Config
+const spacerLen = 15;
+const pollInterval = 5000;
+async function getInfo() {
+console.clear()
+	 const res = await fetch("https://www.aainflight.com/api/v1/connectivity/viasat/flight", {
+	   "headers": {
+	     "accept": "*/*",
+	     "accept-language": "en-US,en;q=0.9",
+	     "if-none-match": "W/\"206-dyu+CkwhoXDkA1zSlovWwVdbBYc\"",
+	     "priority": "u=1, i",
+	     "sec-ch-ua": "\"Not;A=Brand\";v=\"99\", \"Brave\";v=\"139\", \"Chromium\";v=\"139\"",
+	     "sec-ch-ua-mobile": "?0",
+	     "sec-ch-ua-platform": "\"Linux\"",
+	     "sec-fetch-dest": "empty",
+	     "sec-fetch-mode": "cors",
+	     "sec-fetch-site": "same-origin",
+	     "sec-gpc": "1",
+	     "Referer": "https://www.aainflight.com/"
+	   },
+	   "body": null,
+	   "method": "GET"
+	 });
+	 
+	 // Error handling
+	 if (!res.ok) {
+	 	console.log(`${RED} [ERROR]:${RESET} Error fetching flight data. Are you connected to the AA wifi?`);
+	 	console.log(`${BOLD}Error Code:${RESET} ${res.status} ${res.statusText}`)
+	 	const errorText = await res.text();	 	
+	 	console.log(`${BOLD}Error Message:${RESET} ${errorText}`)
+	 	process.exit(1);
+	 }
+	
+	const data = await res.json()
+
+
+	// Format Helpers
+
+	function bold(text) {
+		return `${BOLD}${text}${RESET}`
+	}
+
+
+	// formatted lines
+	const lines = [
+		`${bold(`Flight ${data.flightNumber} (${data.vehicleId})`)}`,
+		`${BOLD}${data.origin}${RESET} to ${BOLD}${data.destination}${RESET}`,
+		`Doors are ${bold(data.doorState)}`,
+		`Time Remaining: ${BOLD}${data.timeToGo} min. ${RESET}`,
+		`Flight Phase: ${bold(data.flightPhase)}`,
+		`${"─".repeat(spacerLen)}`,
+		`${BOLD}Aircraft Data${RESET}`,
+		`Latitude: ${bold(data.latitude)}`,
+		`Longitude: ${bold(data.longitude)}`,
+		`Altitude: ${bold(`${data.altitude}ft`)}`,
+		`Air Temp.: ${bold(data.airTemperature)}`,
+		`Air / Ground speed: ${bold(`${data.airspeed}mph / ${data.groundspeed}mph`)}`,
+
+		
+		
+	]
+
+	lines.forEach(line => {
+		console.log(line)
+	})	 
+};
+
+setInterval(await getInfo, pollInterval)
